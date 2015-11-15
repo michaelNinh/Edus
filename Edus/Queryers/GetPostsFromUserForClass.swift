6ee71c32468fb83baa8155a2c5805b663e240678
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2015-11-15 13:02:50 -0800

    more code into myPOsts

---
+++ Edus/Queryers/GetPostsFromUserForClass.swift
@@ -0,0 +1,34 @@
+//
+//  GetPostsFromUserForClass.swift
+//  Edus
+//
+//  Created by michael ninh on 11/15/15.
+//  Copyright © 2015 Cognitus. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class GetPostsFromUserForClass{
+    
+    static func getPostsForClass(completionBlock: PFQueryArrayResultBlock, classroom: Classroom, range: Range<Int>){
+        
+        let sameClassQuery = PFQuery(className: "Post")
+        //this is where I would modify to create different post category results
+        sameClassQuery.whereKey("toClassroom", equalTo: classroom)
+        
+        
+        let sameUserQuery = PFQuery(className: "Post")
+        sameUserQuery.whereKey("fromUser", equalTo: PFUser.currentUser()!)
+        
+        
+        let postQuery = PFQuery.orQueryWithSubqueries([sameClassQuery, sameUserQuery])
+        postQuery.includeKey("toPostPoints")
+        postQuery.orderByDescending("createdAt")
+        postQuery.skip = range.startIndex
+        postQuery.limit = range.endIndex - range.startIndex
+        postQuery.findObjectsInBackgroundWithBlock(completionBlock)
+        
+    }
+    
+}
---
+++ Edus/viewControllers/MyPostsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MyPostsViewController.swift
+//  Edus
+//
+//  Created by michael ninh on 11/15/15.
+//  Copyright © 2015 Cognitus. All rights reserved.
+//
+
+import UIKit
+
+class MyPostsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
