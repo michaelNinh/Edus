ec151326ec316e332ba26add6601d0adccaa0bb9
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2015-11-15 13:19:12 -0800

    IS EVERYTHING REALLY WORKING? TOO GOOD TO BE TRUE

---
+++ Edus/Views/ClassPostsFromUserTableViewCell.swift
@@ -0,0 +1,90 @@
+//
+//  ClassPostsFromUserTableViewCell.swift
+//  Edus
+//
+//  Created by michael ninh on 11/15/15.
+//  Copyright © 2015 Cognitus. All rights reserved.
+//
+
+import UIKit
+import DateTools
+import Parse
+
+protocol ShowFlagAlertFromUserPost{
+    func showFlagAlert(title: String, message: String, callbackViewCell: ClassPostFromUserTableViewCell)
+    func showDeleteAlert(title: String, message: String, callbackViewCell: ClassPostFromUserTableViewCell)
+}
+
+
+class ClassPostFromUserTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postTitleText: UILabel!
+    @IBOutlet weak var postContentText: UILabel!
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var dateText: UILabel!
+    @IBOutlet weak var scoreText: UILabel!
+    
+    
+    @IBOutlet weak var upVoteButton: UIButton!
+    @IBAction func upVote(sender: AnyObject) {
+        postPoints.toPost = self.post
+        postPoints.upVote()
+        self.upVoteButton.enabled = false
+        scoreText.text = String(Int(scoreText.text!)!+1)
+    }
+    
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBAction func deletePost(sender: AnyObject) {
+        delegate?.showDeleteAlert("", message: "Delete post?", callbackViewCell: self)
+        
+    }
+    
+    
+    @IBAction func flagPost(sender: AnyObject) {
+        delegate?.showFlagAlert("", message: "Flag for inappropriate content?", callbackViewCell: self)
+        
+    }
+    
+    var voterList = [String]()
+    var postPoints = PostPoints()
+    var delegate: ShowFlagAlertFromUserPost?
+    
+    var post: Post?{
+        didSet{
+            if let post = post{
+                dateText.text = post.createdAt?.shortTimeAgoSinceDate(NSDate()) ?? ""
+                postTitleText.text = post.title
+                postContentText.text = post.content
+                nameText.text = post.fromUserName
+                print("the current text value of the score is \(self.scoreText.text)")
+            }
+        }
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func flagContentAction(){
+        let postFlagger = PostFlag()
+        postFlagger.toPost = self.post
+        postFlagger.fromUser = PFUser.currentUser()
+        postFlagger.toUser = self.post?.fromUser
+        postFlagger.flagContent()
+    }
+    
+    func deleteContentAction(){
+        self.post?.deletePost()
+    }
+    
+}
--- Edus/viewControllers/MyPostsViewController.swift
+++ Edus/viewControllers/MyPostsViewController.swift
@@ -7,32 +7,199 @@
 //
 
 import UIKit
+import Parse
+import ConvenienceKit
 
-class MyPostsViewController: UIViewController {
 
+class MyPostsViewController: UIViewController, TimelineComponentTarget, ShowFlagAlertFromUserPost {
+    
+    var classroom: Classroom?
+    var selectedPost: Post?
+    var selectedPostPostPoints: PostPoints?
+    
+    
+    
+    @IBAction func backToHomeSegue(sender: AnyObject) {
+        self.backToHomeSegue()
+    }
+    
+    
     @IBOutlet weak var tableView: UITableView!
+    let defaultRange = 0...4
+    let additionalRangeSize = 5
+    var timelineComponent: TimelineComponent<Post, MyPostsViewController>!
+    
+    //TIMELINE IMPLEMENTATION
+    
+    func loadInRange(range: Range<Int>, completionBlock: ([Post]?) -> Void) {
+        
+        
+        GetPostsFromUserForClass.getPostsForClass({ (result: [PFObject]?, error: NSError?) -> Void in
+            let posts = result as? [Post] ?? []
+            print(posts)
+            completionBlock(posts)
+            }, classroom: self.classroom!, range: range)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        timelineComponent.loadInitialIfRequired()
+        //timelineComponent = TimelineComponent(target: self)
+        
+        
+        
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        //this is how I pass the class data from the tabBar into this controller
+        if self.tabBarController != nil{
+            let tabBarReference = self.tabBarController as! ClassTabBarViewController
+            self.classroom = tabBarReference.classroom
+        }
+        
+        timelineComponent = TimelineComponent(target: self)
+        
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func expandPostSegue(){
+        self.performSegueWithIdentifier("expandPostSegue", sender: self)
     }
-    */
+    
+    func backToHomeSegue(){
+        self.performSegueWithIdentifier("backToHomeSegue", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if (segue.identifier == "expandPostSegue") {
+            let expandedPostViewController = segue.destinationViewController as! ExpandedPostViewController
+            expandedPostViewController.post = selectedPost
+            expandedPostViewController.targetPostPoints = self.selectedPostPostPoints
+        }
+        
+        if (segue.identifier == "makePostSegue") {
+            let makePostViewController = segue.destinationViewController as! MakePostViewController
+            makePostViewController.classroom = self.classroom
+        }
+    }
+    
+    //ALERT STUFF
+    func showFlagAlert(title: String, message: String, callbackViewCell: ClassPostFromUserTableViewCell){
+        let alertController = UIAlertController(title: title , message: message, preferredStyle: .Alert);
+        
+        
+        let yes = UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default){
+            (actionCancel) -> () in
+            print("do the flag")
+            callbackViewCell.flagContentAction()
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel){
+            (actionCancel) -> () in
+            print("action canceled")
+        }
+        
+        alertController.addAction(yes)
+        alertController.addAction(cancel)
+        presentViewController(alertController, animated: true, completion: nil);
+    }
+    
+    
+    
+    
+    func showDeleteAlert(title: String, message: String, callbackViewCell: ClassPostFromUserTableViewCell){
+        let alertController = UIAlertController(title: title , message: message, preferredStyle: .Alert);
+        
+        
+        let yes = UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default){
+            (actionCancel) -> () in
+            print("delete the post")
+            callbackViewCell.deleteContentAction()
+            self.tableView.reloadData()
+            //DELETING DOES NOT UPDATE THE TABLEVIEW...minor issue
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel){
+            (actionCancel) -> () in
+            print("action canceled")
+        }
+        
+        alertController.addAction(yes)
+        alertController.addAction(cancel)
+        presentViewController(alertController, animated: true, completion: nil);
+    }
+    
+}
 
+extension MyPostsViewController: UITableViewDelegate{
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.selectedPost = timelineComponent.content[indexPath.row]
+        expandPostSegue()
+        
+    }
+    
+}
+
+extension MyPostsViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("found this number of posts:\(timelineComponent.content.count)")
+        return timelineComponent.content.count
+        //return 2
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("postCell") as! ClassPostFromUserTableViewCell
+        
+        let post = timelineComponent.content[indexPath.row]
+        post.setPost()
+        cell.post = post
+        cell.delegate = self
+        
+        GetPointsForPost.getPointsForPost({ (result: [PFObject]?, error: NSError?) -> Void in
+            if result!.count != 0{
+                let targetPointObj = result![0] as! PostPoints
+                self.selectedPostPostPoints = targetPointObj
+                targetPointObj.setPoints()
+                cell.scoreText.text = String(targetPointObj.score)
+                
+                if targetPointObj.voterList.contains(PFUser.currentUser()!.objectId!){
+                    print("already voted")
+                    cell.upVoteButton.enabled = false
+                }else{
+                    cell.upVoteButton.enabled = true
+                }
+                
+            }else{
+                print("no votes")
+            }
+            }, post: post)
+        
+        
+        if post.fromUserName == PFUser.currentUser()?.username{
+            print("del enabled")
+            cell.deleteButton.hidden = false
+            cell.deleteButton.enabled = true
+        }else{
+            print("del dis")
+            cell.deleteButton.hidden = true
+            cell.deleteButton.enabled = false
+        }
+        
+        return cell
+    }
 }
